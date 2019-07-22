134bf49758cbd94720bde53f5dc73026d7b6e272
Sampaio Leal <samp14hd@gmail.com>
2019-07-22 03:55:40 -0300

    extended true

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const io = require('socket.io')(server);
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(expressLayouts);
-app.use(bodyParser.urlencoded());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
     res.render('index');
