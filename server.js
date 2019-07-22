a6f5d672f7fa353d9584dd07547a39137e040670
Sampaio Leal <samp14hd@gmail.com>
2019-07-22 05:29:36 -0300

    Comments

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
+//App variables
 const express = require('express')
 const path = require('path')
-
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 
+//App setup
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
 app.use(expressLayouts)
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//Routes
 app.get('*', (req, res, next) => {
     if (req.headers['x-forwarded-proto'] != 'https') {
         res.redirect("https://" + req.headers.host + req.url)
@@ -20,7 +22,6 @@ app.get('*', (req, res, next) => {
         next()
     }
 });
-
 app.get('/', (req, res) => {
     res.render('login')
 })
@@ -31,16 +32,21 @@ app.post('/chat', (req, res) => {
 let messages = []
 var online = 0
 
+//Socket.io configuration
 io.on("connection", socket => {
-    socket.emit('previousMessages', messages)
 
+    //When user connect to app
+    socket.emit('previousMessages', messages)
     online++
     io.emit('counter', { count: online })
 
+    //When user send a message
     socket.on("sendMessage", data => {
         socket.broadcast.emit("receivedMessage", data)
         messages.push(data)
     })
+    
+    //When user disconnect from the app
     socket.on("disconnect", function () {
         online--
         io.emit('counter', { count: online })
