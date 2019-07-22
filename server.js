6c4b4adc710646d6d972d516a7855da75f5a9987
Sampaio Leal <samp14hd@gmail.com>
2019-07-22 04:48:07 -0300

    animal

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
     res.render('login')
 })
 app.post('/chat', (req, res) => {
-    res.render('chat', {username: req.body.user})
+    res.render('chat', {username: req.body.username})
 })
 
 let messages = []
